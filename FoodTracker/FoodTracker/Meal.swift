2dcda683e93f212a94a4b8bb5f4e95723adefd93
YinzhuSU <31676438+YinzhuSU@users.noreply.github.com>
2017-10-10 01:28:16 -0400

    Add files via upload

---
+++ FoodTracker/FoodTracker/Meal.swift
@@ -0,0 +1,25 @@
+//
+//  Meal.swift
+//  FoodTracker
+//
+//  Created by 苏银竹 on 2017/10/9.
+//  Copyright © 2017年 YZSU. All rights reserved.
+//
+
+import UIKit
+
+class Meal {
+    
+    //MARK: Properties
+    
+    var name: String = ""
+    var photo: UIImage?
+    var rating: Int
+    
+}
+
+//MARK: Initialization
+
+init (name: String, photo: UIImage?, rating: Int) {
+    
+}
